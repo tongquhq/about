0663369cd4f72424bb30c22d7711c744e9bf729f
at15 <at15@outlook.com>
2016-09-06 21:09:22 -0700

    Move the logic of cli into lib/cli.js
    - bin/about.js now simply call cli.run()
    - reconfigure logger based on command line arguments

--- bin/about.js
+++ bin/about.js
@@ -1,23 +1,5 @@
 #!/usr/bin/env node
 
 'use strict';
-const meow = require('meow');
-const configLocator = require('../lib/config/locator');
-
-const cli = meow(`
-    Usage
-      $ about <input>
-
-    Options
-      --config, -c  Specify config file
-
-    Examples
-      $ about --config ./example/config.yaml
-      $ about -c ./example/config.yaml
-`, {
-    alias: {
-        c: 'config'
-    }
-});
-console.log(cli.input[0], cli.flags);
-configLocator.detectConfigFile(cli.flags);
+const cli = require('../lib/cli');
+cli.run();
